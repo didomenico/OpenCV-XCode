b619a87d0226f5f64d0d702a35f112f6c0a5c668
William di Domenico <d@idomeni.co>
2020-07-19 12:55:26 -0300

    Initial Commit

---
+++ OpenCV XCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV XCode
+//
+//  Created by Don Toazza on 19/07/20.
+//  Copyright © 2020 Toazza Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
