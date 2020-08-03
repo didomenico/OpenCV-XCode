f89c8ad7022fd7ff8016c7287fd09eff4b0f3648
William di Domenico <d@idomeni.co>
2020-08-02 23:05:13 -0300

    Clean hello world source code for sharing.

--- OpenCV XCode/main.cpp
+++ OpenCV XCode/main.cpp
@@ -1,47 +1,34 @@
 //  main.cpp
 //  OpenCV XCode
 //
-//  Created by Don Toazza on 19/07/20.
+//  Created by William di Domenico
 //  Copyright © 2020 Toazza Labs. All rights reserved.
-//
-
-#include <iostream>
+//  Original from https://gist.github.com/EyalAr/3940636
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/xfeatures2d.hpp>
 
-#include "Veinprint.h"
-#include "Utils.h"
-
-using std::cout;
 using cv::Mat;
+using cv::Scalar;
+using cv::Point;
 
-Veinprint vp = Veinprint();
-
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-    //create a gui window:
-    cv::namedWindow("Output", 1);
-    Mat thresh;
-    //initialize a 120X350 matrix of black pixels:
-    Mat ddl1 = cv::imread("/Users/don/Pictures/ddl1.jpg");
-    Mat ddl2 = cv::imread("/Users/don/Pictures/ddl2.jpg");
+    // Create a gui window:
+    cv::namedWindow("Output",1);
     
-    vector<KeyPoint> keypoints1 = vp.extractKeypoints(ddl1);
-    vector<KeyPoint> keypoints2 = vp.extractKeypoints(ddl2);
+    // Initialize a 400x900 matrix of black pixels:
+    Mat output = Mat::zeros( 400, 900, CV_8UC3 );
     
-    vp.computeDescriptors(ddl1, keypoints1);
-    vp.computeDescriptors(ddl2, keypoints2);
-
-    cv::drawKeypoints(ddl2, keypoints1, ddl2);
+    // Writes text on the matrix:
+    cv::putText(output, "Hello World!", Point(210, 190),
+                cv::QT_FONT_NORMAL, 3, Scalar(0,255,0), 4);
     
-    //display the image:
-    cv::imshow("Output", ddl2);
+    // Display the image:
+    cv::imshow("Output", output);
     
-    //wait for the user to press any key:
+    // Wait for the user to press any key:
     cv::waitKey(0);
     
     return 0;
